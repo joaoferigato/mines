40352cc4629e08825cfc86255cb1fe831ef18bc4
Joao Ferigato <joaoferigato@gmail.com>
2021-10-21 12:49:31 -0300

    creating fields css

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import params from './src/params'
+import Field from './src/components/Field'
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,12 @@ export default class App extends Component {
         <Text style={styles.instructions}>Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
       </View>
     )
   }
